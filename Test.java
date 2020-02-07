99ef1c09e4985fd758115592195987a291b3a378
Jagadeesh Kumar <49532394+JagIIHT@users.noreply.github.com>
2020-02-07 11:13:52 +0530

    Update Test.java

--- Test.java
+++ Test.java
@@ -30,3 +30,96 @@ public class LibraryUserDetailsServiceTest {
 		assertThat(lib.getUserId().equals(this.lib.getUserId()));
 	}
 }
+
+
+
+@RunWith(SpringRunner.class)
+public class LibrarianServiceTest {
+
+	@TestConfiguration
+	static class LibrarianServiceTestContextConfiguration {
+
+		@Bean
+		public LibrarianService librarianService () {
+			return new LibrarianServiceImpl();
+		}
+	}
+
+	@MockBean
+	private LibrarianRepository librarianRepository;
+
+	@Autowired
+	private LibrarianService librarianService;
+
+	private Book book;
+	
+	@Before
+	public void setUp() {
+		List<Book> allList = Arrays.asList(new Book("Java","Y"),new Book("C","N"));
+		List<Book> issuedList = Arrays.asList(new Book("Java","Y"));
+		this.book = new Book("Java", "Y");
+		Mockito.when(this.librarianRepository.findAll()).thenReturn(allList);
+		Mockito.when(this.librarianRepository.save(this.book)).thenReturn(this.book);
+		Mockito.when(this.librarianRepository.findAllByIssued("Y")).thenReturn(issuedList);
+	}
+	
+	@Test
+	public void testAddBook() {
+		Book b = this.librarianService.addBook(this.book);
+		assertThat(b.getName().equals(this.book.getName()));
+	}
+	@Test
+	public void testGetAllBooks() {
+		List<Book> list = this.librarianService.getAllBooks();
+		assertThat(list.size() == 2);
+		assertThat(list.get(0).getName().equals("Java"));
+		assertThat(list.get(1).getName().equals("C"));
+	}
+	
+	@Test
+	public void testGetAllIssuedBooks() {
+		List<Book> list = this.librarianService.getAllIssuedBooks();
+		assertThat(list.size() == 1);
+		assertThat(list.get(0).getName().equals("Java"));
+	}
+	
+	@Test
+	public void testIssueBook_Success() {
+		Mockito.when(this.librarianRepository.findById(1)).thenReturn(Optional.of(new Book("Java", "N")));
+		Book b = this.librarianService.issueBook(new Book("Java", "N"));
+		assertThat(b.getIssued().equals("Y"));
+	}
+	
+	@Test(Exception = LibraryException.class)
+	public void testIssueBook_Failure1() {
+		Mockito.when(this.librarianRepository.findById(1)).thenReturn(Optional.of(new Book("Java", "Y")));
+		this.librarianService.issueBook(new Book("Java", "N"));
+	}
+	
+	@Test(Exception = LibraryException.class)
+	public void testIssueBook_Failure2() {
+		Mockito.when(this.librarianRepository.findById(1)).thenReturn(Optional.of(null));
+		this.librarianService.issueBook(new Book("Java", "N"));
+	}
+	
+	@Test
+	public void testReturnBook_Success() {
+		Mockito.when(this.librarianRepository.findById(1)).thenReturn(Optional.of(new Book("Java", "Y")));
+		Mockito.when(this.librarianRepository.save(new Book("Java", "Y"))).thenReturn(new Book("Java", "N");
+		Book b = this.librarianService.issueBook(new Book("Java", "Y"));
+		assertThat(b.getIssued().equals("N"));
+		
+	}
+											      
+	@Test(Exception = LibraryException.class)
+	public void testReturnBook_Failure1() {
+		Mockito.when(this.librarianRepository.findById(1)).thenReturn(Optional.of(new Book("Java", "N")));
+		this.librarianService.issueBook(new Book("Java", "Y"));		
+	}
+											      
+	@Test(Exception = LibraryException.class)
+	public void testReturnBook_Failure2() {
+		Mockito.when(this.librarianRepository.findById(1)).thenReturn(Optional.of(null));
+		this.librarianService.issueBook(new Book("Java", "Y"));		
+	}
+}
